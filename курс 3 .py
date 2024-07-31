2d76209de2c548b356ccbce58e0c073157d4fe93
Daniil-svg814 <abduhalikovdaniil@gmail.com>
2024-07-31 23:48:01 +0300

    Add files via upload

--- курс 3 .py
+++ курс 3 .py
@@ -1,5 +1,5 @@
 home_work = 12
 namber_of_hours_spent = 1.5
 coutse_name = 'python'
-watch = 8.0
+watch = 0.125
 print("курс:" , coutse_name, "всего задач:", home_work, 'затраченно часов:', namber_of_hours_spent, "среднее выполение:", watch )
